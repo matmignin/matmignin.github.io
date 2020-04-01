9f2f3b673e08fe392a2d15bcd395cff16ad9d35b
mathough <mathough@gmail.com>
2020-04-01 14:59:42 -0400

    initial commit

--- assets/js/untitled.js
+++
@@ -1,7 +0,0 @@
-$(".card-header").parent('.card').hover(
-  function() {
-    $(this).children('.collapse').collapse('show');
-  }, function() {
-    $(this).parent('.collapse').collapse('hide');
-  }
-);
